99d891c372d73ae828c839152c9d57291770e17c
mrp089 <51269309+mrp089@users.noreply.github.com>
2021-08-02 20:00:25 -0700

    build with current master

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 #source /opt/intel/oneapi/setvars.sh
-#g++ -fopenmp -fPIC -shared -o FEMbeCmm.o FEMbeCmm.cpp FEMbeCmm.h -I/home/pfaller/work/marcos/FEBio2.8.2/sdk/include/
-g++ -fopenmp -fPIC -shared -o FEMbeCmm.o *.cpp *.h -I/home/pfaller/work/marcos/FEBio3 -L/home/pfaller/febio-2.8.0/sdk/lib/ -lfebiomech_gcc64 -lfecore_gcc64
-#icpc -qopenmp -fPIC -shared -o FEMbeCmm.o FEMbeCmm.cpp FEMbeCmm.h -I/home/pfaller/work/marcos/FEBio3
+icpc -fopenmp -fPIC -shared -o FEMbeCmm.o *.cpp *.h -I/home/pfaller/work/repos/FEBio_master -L/home/pfaller/work/repos/FEBio_master/build/lib -lfebiomech_lnx64 -lfecore_lnx64
